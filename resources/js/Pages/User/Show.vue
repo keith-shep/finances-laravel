bb23ade89e76051035bbde3f1da731d4531856fc
Keith Shepherdson <kmson7@gmail.com>
2024-10-01 23:13:29 +0800

    Remove uppercase

--- resources/js/Pages/User/Show.vue
+++ resources/js/Pages/User/Show.vue
@@ -3,7 +3,7 @@
     <Head title="finances" />
 
     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
                 <th class="px-6 py-3">Transaction Date</th>
                 <th class="px-6 py-3">Reference</th>
