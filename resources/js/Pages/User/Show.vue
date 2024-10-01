bc33d7f8697e1ef7c28a88f9c9aec63cb8bd784b
Keith Shepherdson <kmson7@gmail.com>
2024-10-01 23:12:25 +0800

    Add table styling

--- resources/js/Pages/User/Show.vue
+++ resources/js/Pages/User/Show.vue
@@ -2,30 +2,30 @@
   <!-- <Layout> -->
     <Head title="finances" />
 
-    <table class="border">
-        <thead>
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-                <th>Transaction Date</th>
-                <th>Reference</th>
-                <th>Credit Amount</th>
-                <th>Debit Amount</th>
-                <th>Ref1</th>
-                <th>Ref2</th>
-                <th>Ref3</th>
-                <th>Description</th>
+                <th class="px-6 py-3">Transaction Date</th>
+                <th class="px-6 py-3">Reference</th>
+                <th class="px-6 py-3">Credit Amount</th>
+                <th class="px-6 py-3">Debit Amount</th>
+                <th class="px-6 py-3">Ref1</th>
+                <th class="px-6 py-3">Ref2</th>
+                <th class="px-6 py-3">Ref3</th>
+                <th class="px-6 py-3">Description</th>
             </tr>
         </thead>
 
         <tbody>
-            <tr class="border" v-for="transaction in transactions">
-                <td>{{ transaction.transaction_date }}</td>
-                <td>{{ transaction.reference }}</td>
-                <td>{{ transaction.credit_amount }}</td>
-                <td>{{ transaction.debit_amount }}</td>
-                <td>{{ transaction.ref1 }}</td>
-                <td>{{ transaction.ref2 }}</td>
-                <td>{{ transaction.ref3 }}</td>
-                <td>{{ transaction.description }}</td>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700" v-for="transaction in transactions">
+                <td class="px-6 py-3">{{ transaction.transaction_date }}</td>
+                <td class="px-6 py-3">{{ transaction.reference }}</td>
+                <td class="px-6 py-3">{{ transaction.credit_amount }}</td>
+                <td class="px-6 py-3">{{ transaction.debit_amount }}</td>
+                <td class="px-6 py-3">{{ transaction.ref1 }}</td>
+                <td class="px-6 py-3">{{ transaction.ref2 }}</td>
+                <td class="px-6 py-3">{{ transaction.ref3 }}</td>
+                <td class="px-6 py-3">{{ transaction.description }}</td>
             </tr>
         </tbody>
     </table>
