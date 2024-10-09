51220f050387f0c7b5ca0744df71bbf5c476f189
Keith Shepherdson <kmson7@gmail.com>
2024-10-10 00:24:22 +0800

    WIP on importing

--- app/Services/TransactionService.php
+++ app/Services/TransactionService.php
@@ -4,7 +4,9 @@ namespace App\Services;
 
 use App\Models\Transaction;
 use Carbon\Carbon;
+use Exception;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\DB;
 
 class TransactionService
 {
@@ -17,41 +19,46 @@ class TransactionService
     }
 
 
-    public function importCsv(UploadedFile $file){
+    public function importCsv(UploadedFile $file): bool {
         $entries = array_map('str_getcsv', file($file));
         $STARTING_LINE = 18;
-        foreach ($entries as $index => $entry) {
-          
 
-            if ($index < $STARTING_LINE) {
-                continue;
+        DB::beginTransaction();
+        try{
+            throw new Exception('smth wrong!');
+            foreach ($entries as $index => $entry) {
+                if ($index < $STARTING_LINE) {
+                    continue;
+                }
+                if (!isset($entry[0])) {
+                    continue;
+                }
+
+                $date = Carbon::parse($entry[0]);
+                $reference = $entry[1] ?? '';
+                $debit_amount = isset($entry[2]) ? (float) $entry[2] : 0;
+                $credit_amount = isset($entry[3]) ? (float) $entry[3] : 0;
+                $ref1 = $entry[4] ?? '';
+                $ref2 = $entry[5] ?? '';
+                $ref3 = $entry[6] ?? '';
+
+                
+                $transaction = Transaction::create([
+                    'transaction_date' => $date,
+                    'reference' => $reference,
+                    'credit_amount' => $credit_amount,
+                    'debit_amount' => $debit_amount,
+                    'ref1' => $ref1,
+                    'ref2' => $ref2,
+                    'ref3' => $ref3,
+                ]);
             }
-            if (!isset($entry[0])) {
-                continue;
-            }
-
-            $date = Carbon::parse($entry[0]);
-            $reference = $entry[1] ?? '';
-            $debit_amount = isset($entry[2]) ? (float) $entry[2] : 0;
-            $credit_amount = isset($entry[3]) ? (float) $entry[3] : 0;
-            $ref1 = $entry[4] ?? '';
-            $ref2 = $entry[5] ?? '';
-            $ref3 = $entry[6] ?? '';
-
-            
-            $transaction = Transaction::create([
-                'transaction_date' => $date,
-                'reference' => $reference,
-                'credit_amount' => $credit_amount,
-                'debit_amount' => $debit_amount,
-                'ref1' => $ref1,
-                'ref2' => $ref2,
-                'ref3' => $ref3,
-            ]);
-          
-            
+            DB::commit();
+            return true;
+        } catch (Exception $e) {
+            DB::rollback();
+            throw $e;
         }
-        
     }
 
    
