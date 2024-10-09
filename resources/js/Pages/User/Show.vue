1d67781b9764d58bca4aaaac5c9dcb64ce1b6211
Keith Shepherdson <kmson7@gmail.com>
2024-10-09 23:20:18 +0800

    Swap attributes

--- resources/js/Pages/User/Show.vue
+++ resources/js/Pages/User/Show.vue
@@ -36,7 +36,7 @@
                 </template>
 
                 <tbody v-else>
-                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700" v-for="transaction in transformedData">
+                    <tr v-for="transaction in transformedData" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700" >
                         <td class="px-6 py-3">{{ transaction.transaction_date }}</td>
                         <td class="px-6 py-3">{{ transaction.reference }}</td>
                         <td class="px-6 py-3">{{ transaction.debit_amount }}</td>
