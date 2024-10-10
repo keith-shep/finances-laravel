32e8a6b5b1bb6230c491e073e53f6c37cc802b54
Keith Shepherdson <kmson7@gmail.com>
2024-10-10 22:10:28 +0800

    Add colors

--- app/Services/TransactionService.php
+++ app/Services/TransactionService.php
@@ -70,7 +70,10 @@ class TransactionService
         }
 
         $datasets = [
-            ['data' => $data]
+            [
+                'data' => $data,
+                'backgroundColor' => ['#41B883', '#E46651', '#00D8FF', '#DD1B16']
+            ]
         ];
         
         return [
