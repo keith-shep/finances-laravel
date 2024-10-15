f773d8379efb5bc0ae96972d960f2b388d4d83a1
Keith Shepherdson <kmson7@gmail.com>
2024-10-15 23:11:47 +0800

    Add checkbox

---
+++ resources/js/Components/Checkbox.vue
@@ -0,0 +1,25 @@
+<template>    
+    <form>
+        <template v-for="option in options">
+            <input type="checkbox" :value="option.value" v-model="form.checkedCategoryIds" />
+            <label for="jack">{{ option.name }}</label>
+        </template>
+        
+    </form>
+</template>
+
+<script setup>
+
+import { reactive, computed } from 'vue'
+import { useForm } from '@inertiajs/vue3'
+
+const props = defineProps({ 
+    options: Array,
+})
+
+
+const form = useForm({  
+    checkedCategoryIds: []
+})
+
+</script>
