fb361a5fec80485a27e5dad39d17fa6dcd0b6b97
Keith Shepherdson <kmson7@gmail.com>
2024-10-15 23:19:53 +0800

    Group filters together

--- resources/js/Components/Checkbox.vue
+++ resources/js/Components/Checkbox.vue
@@ -1,11 +1,23 @@
 <template>    
-    <form>
+    
+    <form @submit.prevent="form.get('/finances')">
+        <label class="block" for="start">Start date:</label>
+        <input type="month" id="start" name="from" v-model="form.from" min="2024-01" max="2024-12" />
+        
+        <label class="block" for="end">End date:</label>
+        <input type="month" id="end" name="to" v-model="form.to" min="2024-01" max="2024-12" />
+
+        
         <template v-for="option in options">
-            <input type="checkbox" :value="option.value" v-model="form.checkedCategoryIds" />
+            <input type="checkbox" :value="option.value" v-model="form.category_ids" />
             <label for="jack">{{ option.name }}</label>
         </template>
+
+        <!-- submit -->
+        <button class="block bg-blue-500 text-white rounded-full py-2 px-4" type="submit" :disabled="form.processing">Filter</button>
         
     </form>
+    
 </template>
 
 <script setup>
@@ -19,7 +31,10 @@ const props = defineProps({
 
 
 const form = useForm({  
-    checkedCategoryIds: []
+    from: props.from,
+    to: props.to,
+    category_ids: []
 })
+
 
 </script>
--- resources/js/Pages/Transaction/Index.vue
+++ resources/js/Pages/Transaction/Index.vue
@@ -4,18 +4,7 @@
 
     <div class="grid grid-cols-6 gap-4 ">
         <aside class="col-span-2">
-            <form @submit.prevent="form.get('/finances')">
-                <label class="block" for="start">Start date:</label>
-                <input type="month" id="start" name="from" v-model="form.from" min="2024-01" max="2024-12" />
-                
-                <label class="block" for="end">End date:</label>
-                <input type="month" id="end" name="to" v-model="form.to" min="2024-01" max="2024-12" />
-
-                <!-- submit -->
-                <button class="block bg-blue-500 text-white rounded-full py-2 px-4" type="submit" :disabled="form.processing">Filter</button>
-            </form>
-
-
+            
             <Checkbox :options="categories.data"/>
             <ImportCsv />
 
