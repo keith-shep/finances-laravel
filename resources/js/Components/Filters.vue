8e9d6b782f2e4e34a0ef77975274c9dd7cefba3d
Keith Shepherdson <kmson7@gmail.com>
2024-10-20 15:20:56 +0800

    Use onchange instead of submit

--- resources/js/Components/Filters.vue
+++ resources/js/Components/Filters.vue
@@ -1,20 +1,18 @@
 <template>    
     
-    <form @submit.prevent="form.get('/finances')">
+    <form>
         <label class="block" for="start">Start date:</label>
-        <input type="month" id="start" name="from" v-model="form.from" min="2024-01" max="2024-12" />
+        <input type="month" id="start" name="from" v-model="form.from" min="2024-01" max="2024-12" @change="form.get('/finances')"/>
         
         <label class="block" for="end">End date:</label>
-        <input type="month" id="end" name="to" v-model="form.to" min="2024-01" max="2024-12" />
+        <input type="month" id="end" name="to" v-model="form.to" min="2024-01" max="2024-12" @change="form.get('/finances')"/>
 
         
         <template v-for="option in options">
-            <input type="checkbox" :value="option.value" v-model="form.category_ids" />
+            <input type="checkbox" :value="option.value" v-model="form.category_ids"  @change="form.get('/finances')"/>
             <label for="jack">{{ option.name }}</label>
         </template>
 
-        <!-- submit -->
-        <button class="block bg-blue-500 text-white rounded-full py-2 px-4" type="submit" :disabled="form.processing">Filter</button>
         
     </form>
     
@@ -38,6 +36,10 @@ const form = useForm({
     to: props.to,
     category_ids: props.category_ids
 })
+
+function test() {
+    alert('test');
+}
 
 
 </script>
