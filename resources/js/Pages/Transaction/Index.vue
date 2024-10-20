19cba90897694bbb0472b004fa2b5a1ef46c78b4
Keith Shepherdson <kmson7@gmail.com>
2024-10-20 16:17:50 +0800

    Add Nowrap

--- resources/js/Pages/Transaction/Index.vue
+++ resources/js/Pages/Transaction/Index.vue
@@ -20,7 +20,7 @@
             <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                 <thead class="text-xs text-gray-700 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
-                        <th class="px-6 py-3">Transaction Date</th>
+                        <th class="px-6 py-3 whitespace-nowrap">Transaction Date</th>
                         <th class="px-6 py-3">Reference</th>
                         <th class="px-6 py-3">Debit Amount</th>
                         <th class="px-6 py-3">Credit Amount</th>
