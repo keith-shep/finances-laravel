7ac8377cff3a2a6cca9da4178167922878d63e34
Keith Shepherdson <kmson7@gmail.com>
2024-11-09 11:16:28 +0800

    Add htmx

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <script src="https://unpkg.com/htmx.org@2.0.3" integrity="sha384-0895/pl2MU10Hqc6jd4RvrthNlDiE9U1tWmX7WRESftEDRosgxNsQG/Ze9YMRzHq" crossorigin="anonymous"></script>
     @vite('resources/js/app.js')
     @vite('resources/css/app.css')
     @inertiaHead
