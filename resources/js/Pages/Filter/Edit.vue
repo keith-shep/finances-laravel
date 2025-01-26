060e7a9dc4c9747219b5b83c42419ed26f226d89
Keith Shepherdson <kmson7@gmail.com>
2025-01-26 21:46:38 +0800

    Add back btn

--- resources/js/Pages/Filter/Edit.vue
+++ resources/js/Pages/Filter/Edit.vue
@@ -1,6 +1,10 @@
 <template>
     <Head :title="filter.name" />
 
+    
+
+    <Link class="block bg-blue-500 text-white rounded-full py-2 px-4" :href="`/categories/${filter.category_id}`" as="button">Back</Link>
+
     <h1>{{ filter.name }}</h1> 
     <div class="grid grid-cols-6 gap-4 ">
         <div class="col-span-4">
@@ -24,7 +28,7 @@
 </template>
 
 <script setup>
-import { Head, useForm } from '@inertiajs/vue3'
+import { Head, useForm, Link } from '@inertiajs/vue3'
 
 const props = defineProps({ 
     filter: Object
