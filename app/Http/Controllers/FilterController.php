a460dd720ad7e6072e9a11678d0fb03347f7673c
Keith Shepherdson <kmson7@gmail.com>
2025-02-03 20:35:55 +0800

    Casting category

--- app/Http/Controllers/FilterController.php
+++ app/Http/Controllers/FilterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\CategoryFilter;
 use App\Models\Transaction;
 use Illuminate\Http\Request;
@@ -20,15 +21,15 @@ class FilterController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create($category_id)
+    public function create(Category $category)
     {
         return Inertia::render('Filter/Edit', [
-            'category_id' => (int) $category_id,
+            'category_id' => (int) $category->id,
         ]);
     }
 
      
-    public function store(Request $request, int $category_id)
+    public function store(Request $request, Category $category)
     {
         try {
             $validated = $request->validate([
@@ -37,11 +38,11 @@ class FilterController extends Controller
             ]);
             
             CategoryFilter::create([
-                'category_id' => $category_id,
+                'category_id' => $category->id,
                 'column_name' => $validated['column_name'],
                 'pattern' => $validated['pattern'],
             ]);
-            return to_route('categories.show', ['category' => $category_id], 303);
+            return to_route('categories.show', ['category' => $category->id], 303);
         } catch (\Exception $e) {
             dd($e);
         }   
@@ -55,30 +56,30 @@ class FilterController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit($category_id, $filter_id)
+    public function edit(Category $category, $filter_id)
     {
         $filter = CategoryFilter::find($filter_id);
         return Inertia::render('Filter/Edit', [
             'filter' => $filter,
-            'category_id' => $category_id,
+            'category_id' => $category->id,
         ]);
     }
 
-    public function update(Request $request, int $category_id, int $filter_id)
+    public function update(Request $request, Category $category, int $filter_id)
     {
         $filter = CategoryFilter::find($filter_id);
         $filter->update([
             'column_name' => $request->column_name,
             'pattern' => $request->pattern,
         ]);
-        return to_route('categories.show', ['category' => $category_id], 303);
+        return to_route('categories.show', ['category' => $category->id], 303);
     }
 
-    public function destroy(int $category_id, int $filter_id)
+    public function destroy(Category $category, int $filter_id)
     {
         $filter = CategoryFilter::find($filter_id);
         $filter->delete();
 
-        return to_route('categories.show', ['category' => $category_id], 303);
+        return to_route('categories.show', ['category' => $category->id], 303);
     }
 }
